9e913a7a3148b899f0a14d4c2735e529034a7500
danielequinzio <daniquinzio2@gmail.com>
2024-04-22 16:44:24 +0200

    commit di prova

--- app/src/main/java/com/example/piadinaparty/MainActivity.kt
+++ app/src/main/java/com/example/piadinaparty/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-//matteo fico
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
