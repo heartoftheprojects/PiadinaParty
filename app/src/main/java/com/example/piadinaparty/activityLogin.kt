2a88f83df1366ab0c4e9c94cd85812b1ed8f0e6a
heartoftheprojects <107889250+heartoftheprojects@users.noreply.github.com>
2024-12-24 14:56:14 +0100

    activity login

--- app/src/main/java/com/example/piadinaparty/activityLogin.kt
+++ app/src/main/java/com/example/piadinaparty/activityLogin.kt
@@ -2,26 +2,49 @@ package com.example.piadinaparty
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.piadinaparty.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
-class activityLogin : AppCompatActivity(){
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_login)
 
-            val btnAccedi = findViewById<Button>(R.id.Accedi)
+class activityLogin : AppCompatActivity() {
 
-            btnAccedi.setOnClickListener {
-                val intent = Intent(this, activityHome::class.java)
-                startActivity(intent)}
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            val btnRegistrati = findViewById<Button>(R.id.Registrati)
+        firebaseAuth = FirebaseAuth.getInstance()
 
-            btnRegistrati.setOnClickListener {
-                val intent = Intent(this, activityRegistrazione::class.java)
-                startActivity(intent)
-                finish()
+        binding.loginButton.setOnClickListener{
+            val email = binding.loginEmail.text.toString()
+            val password = binding.loginPassword.text.toString()
+
+            if(email.isNotEmpity() && password.isNotEmpity()){
+                firebaseAuth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(this){task ->
+                        if (task.isSuccessful){
+                            Toast.makeText(this,"Login avvenuto correttamente!",Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this,MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }else{
+                            Toast.makeText(this,"Login fallito!",Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+            }else{
+                Toast.makeText(this,"Inserire Email e Password",Toast.LENGTH_SHORT).show()
+
             }
         }
+        binding.signupText.setOnClickListener{
+            startActivity(Intent(this,activityRegistrazione::class.java))
+            finish()
+        }
+    }
 }
